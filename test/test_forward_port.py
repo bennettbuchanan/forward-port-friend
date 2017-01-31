a5e7c0ee082c4feeb456cae8785efbad25aae56c
Bennett Buchanan <bennett.buchanan@scality.com>
2017-01-31 00:47:37 -0800

    Add repo cloning test

--- test/test_forward_port.py
+++ test/test_forward_port.py
@@ -1,5 +1,9 @@
-def inc(x):
-    return x + 1
+import os
+from utils.repo_handler import get_repo_object
+from utils.parser import parse_args
 
-def test_answer():
-    assert inc(3) == 4
+def test_get_repo_object():
+    (options, args) = parse_args([])
+    repo = get_repo_object("utapi", "master", options)
+    # The current working directory is the root of this project.
+    assert os.path.isdir("./utapi"), "Repository not cloned to project root."
